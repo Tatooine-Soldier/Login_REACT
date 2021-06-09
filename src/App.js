1d03e636db6f3fdc8acf9566b6ec019ac073f5cf
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-06-09 11:32:51 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,16 +4,34 @@ import './App.css';
 import LoginForm from './components/LoginForm.js'
 
 function App() {
+  const adminUser = {
+    username: "tj@gmail.com",
+    password: "tod123"
+  }
   const [user, setUser] = useState({username:"",password:""})
+  const [error, setError] = useState("")
+
   const Login = (details) => {
     console.log('login in app')
-    setUser({username: details.username, password: details.password})
+    if (details.username === adminUser.username && details.password === adminUser.password) {
+      console.log('Logged In')
+      setUser({username: details.username, password: details.password})
+    } else {
+      console.log('FAIL')
+      setError("FAIL incorrect username or password")
+    }
+  }
+
+  const Logout = () => {
+    console.log('logout')
+    setUser({username: "", password: ""})
   }
   
 
   return (
     <div className="App">
-      {(user.username === "")?(<LoginForm />):(<div>Welcome <Login/></div>)}
+      {(user.username != "")?(<div>Hey {user.username} <span><button onClick={Logout}>Logout</button></span></div>)
+      :(<LoginForm Login={Login} error={error}/>)}
     </div>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,29 @@
 import {React, useState} from 'react'
-import Login from './Login.js'
 
 
-export default function LoginForm() {
+
+export default function LoginForm({Login, error}) {
     const [details, setDetails] = useState({username:"",password:""})
     const handleSubmit = e => {
         e.preventDefault()
-        {Login}
+        console.log(details)
+        //setDetails({...details, username:details.username, password:details.password})
+        Login(details)
     }
-    console.log(details)
 
     return (
         <div id='loginform'>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder="username" name="username" onChange={e => setDetails({...details, username:e.target.value})} value={details.name}/>
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder="password" name="password" onChange={e => setDetails({...details, password:e.target.value})} value={details.password}/>               
-                <input type="submit"  id="sub_form"/>
+            {(error != "") ? (<div>{error}</div>):""}
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" placeholder="username" name="username" onChange={e => setDetails({...details, username:e.target.value})} value={details.name} required/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="password" name="password" onChange={e => setDetails({...details, password:e.target.value})} value={details.password}/>               
+                    <input type="submit"  id="sub_form"/>
+                </div>
             </form>
+            
             
         </div>
     )
